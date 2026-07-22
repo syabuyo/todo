c77004a2ce0d7cf536754bc5575a21285e2c54fb
Kazuya <kazuya2002uk@gmail.com>
2026-07-23 00:43:31 +0900

    get エンドポンと作成

--- todo_server/src/app.ts
+++ todo_server/src/app.ts
@@ -1,11 +1,24 @@
 import express from "express"
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 const handleListen = (() =>{
 	console.log("hello my name is \n", PORT);
 	console.log("server is running on port =", PORT);
+})
+
+
+type Todo = {
+	id:number,
+	title: string,
+	completed: boolean
+};
+type Todos = Todo[];
+const mockTodo:Todos = [{id:1, title: "買い物", completed:false}]
+
+app.get("/todos", (req, res)=>{
+	res.json(mockTodo);
 })
 
 app.listen(PORT, handleListen);
